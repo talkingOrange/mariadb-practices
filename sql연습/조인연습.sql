cafc8cdadae0b3157f344f6f990ca8952fa33e61
[talkingOrange] <sonrisa-bonita@naver.com>
2023-08-30 15:42:56 +0900

    [ feat ] 조인 연습

---
+++ sql연습/조인연습.sql
@@ -0,0 +1,10 @@
+-- 
+-- inner join
+--
+
+-- 예1) 현재 근무하고 있는 직원 사번과, 이름, 직책을 모두 출력
+select a.emp_no, a.first_name, b.title
+from employees a, titles b
+where a.emp_no = b.emp_no -- join 조건 (n-1)
+and b.to_date = '9999-01-01'; -- row 선택 조건
+ 
